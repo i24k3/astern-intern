a9daa61d11179dbc8f5679b87e2011b2e093b3d6
izake <akha3743@gmail.com>
2025-04-16 09:49:27 +0000

    testing out express

--- express-test/app.js
+++ express-test/app.js
@@ -1,27 +1,49 @@
 const express = require("express");
 
 const server = express();
-
+/*
 server.get("/abc/*splat", (req, res, next) => {
-  console.log("GET abc");
+  console.log("GET /abc/*");
   next();
-  console.log("3")
-//   res.end("<h1>abc path</h1>");
+  console.log('get next /abc....');
+});
+*/
+
+server.get("/", (req, res) => {
+  console.log("1", req.params);
+  res.send("/")
 })
 
-server.get("/abc/def", (req, res) => {
-    console.log("GET abc 2");
-    // res.end("<h1>abc path 2</h1>");
-  })
-
-server.post("/abc", (req, res) => {
-  console.log("POST abc");
-  res.end('post req -rout post');
+server.get("/posts/muneeb/into-to-node/view", (req, res, next ) => {
+  console.log("2", req.params);
+  next(); 
+  console.log("special easter egg response")
 })
 
-server.get("/def", (req, res) => {
-  console.log("GET def");
-  res.end("<h1>def path</h1>");
+server.get("/posts/:userid/:postId/view", (req, res) => {
+  console.log("3", req.params);
+  res.send("response")
+  //res.socket.end();
 })
+
+//server.get("/abc/def", (req, res, next) => {
+//  console.log("GET abc/ - 2");
+//  next();
+//  console.log("end")
+//  res.end("<h1>get /abc/ - 2</h1>");
+//});
+//
+//server.post("/def", (req, res) => {
+//  console.log("POST abc");
+//  res.end('post req -rout post');
+//});
+//
+//server.get("/abc/def", (req, res) => {
+//  console.log("GET abc/def");
+//  //res.end("<h1>get /abc/def</h1>");
+//});
+
+
+
 
 server.listen(3000)
